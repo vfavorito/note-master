883267617d117e474c6fa540640bc332a7b81985
vfavorito <vince.favorito@gmail.com>
2020-12-12 15:15:50 -0600

    added /api/notes route

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // bringing in dependencies 
 let express = require("express");
 let path = require("path");
+
 // defining server variables
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 // bininging in middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
 // start the server listining on PORT
 app.listen(PORT,function(){
     console.log("Server launched on PORT:" + PORT);
 });
+
+//json object array
+let notes = [];
 
 //HTML routes
 app.get("/", function(req,res){
@@ -18,4 +24,9 @@ app.get("/", function(req,res){
 });
 app.get("/notes", function(req,res){
     res.sendFile(path.join(__dirname,"public/notes.html"));
+});
+
+//api routes
+app.get("/api/notes", function(req,res){
+    return res.json(notes);
 });
